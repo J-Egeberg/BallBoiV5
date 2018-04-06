d7c3a0b7b9f4333b0029ecf2a87fb97f0a65dbb0
J-Egeberg <jregeberg@gmail.com>
2018-04-06 14:17:28 +0200

    added fragments.. missing link

---
+++ app/src/main/java/oakberg/dk/mytemplate/activities/SectionStatePagerAdapter.java
@@ -0,0 +1,26 @@
+package oakberg.dk.mytemplate.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Oakberg on 06/04/2018.
+ */
+
+public class SectionStatePagerAdapter extends FragmentStatePagerAdapter {
+
+    public SectionStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
