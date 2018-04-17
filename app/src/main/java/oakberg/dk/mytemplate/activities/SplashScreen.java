206a34c3272341ea4ba70f844cb21e3a857242aa
cymback <caspergrozz@gmail.com>
2018-04-17 17:42:57 +0200

    Added splashscreen its working

--- app/src/main/java/oakberg/dk/mytemplate/activities/SplashScreen.java
+++ app/src/main/java/oakberg/dk/mytemplate/activities/SplashScreen.java
@@ -3,7 +3,6 @@ package oakberg.dk.mytemplate.activities;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.style.EasyEditSpan;
 import android.view.View;
 
 import gr.net.maroulis.library.EasySplashScreen;
@@ -18,22 +17,22 @@ public class SplashScreen extends AppCompatActivity {
         EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
                 .withFullScreen()
                 .withTargetActivity(MainActivity.class)
-                .withSplashTimeOut(5000)
-                .withBackgroundColor(Color.parseColor("#074E72"))
-                .withLogo(R.mipmap.ic_launcher)
-                .withHeaderText("Food on a budget")
-                .withFooterText("Copyright 2018 - GROSS GROSS GROSS")
-                .withBeforeLogoText("CPH Dev Co, Ltd")
-                .withAfterLogoText("Satisfy your needs!");
+                .withSplashTimeOut(100000000)
+                .withBackgroundColor(Color.parseColor("#ACACAC"))
+                .withLogo(R.drawable.cheap)
+                .withHeaderText("")
+                .withFooterText("Copyright 2018 - CPH Business")
+                .withBeforeLogoText("")
+                .withAfterLogoText("");
 
 
                 // SET TEXT COLOR
 
 
-                config.getHeaderTextView().setTextColor(Color.WHITE);
-                config.getFooterTextView().setTextColor(Color.WHITE);
-                config.getAfterLogoTextView().setTextColor(Color.WHITE);
-                config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+                config.getHeaderTextView().setTextColor(Color.BLACK);
+                config.getFooterTextView().setTextColor(Color.BLACK);
+                config.getAfterLogoTextView().setTextColor(Color.BLACK);
+                config.getBeforeLogoTextView().setTextColor(Color.BLACK);
 
                 // SET TO VIEW
 
