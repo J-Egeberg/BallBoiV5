5d08fcd0da0870b4f507e4ad2209ac09d106f93c
cymback <caspergrozz@gmail.com>
2018-04-26 14:13:36 +0200

    added login signup, saving user information

--- app/src/main/java/oakberg/dk/mytemplate/activities/MainActivity.java
+++ app/src/main/java/oakberg/dk/mytemplate/activities/MainActivity.java
@@ -1,9 +1,25 @@
 package oakberg.dk.mytemplate.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import oakberg.dk.mytemplate.R;
 import oakberg.dk.mytemplate.fragment.Exit;
@@ -12,18 +28,48 @@ import oakberg.dk.mytemplate.fragment.Login;
 import oakberg.dk.mytemplate.fragment.Menu;
 import oakberg.dk.mytemplate.fragment.MyRecipes;
 import oakberg.dk.mytemplate.fragment.AllRecipes;
+import oakberg.dk.mytemplate.fragment.ProfileActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private static final String TAG = "MainActivity";
     private static SectionStatePagerAdapter sectionStatePagerAdapter;
     private ViewPager viewPager;
 
+
+    /* FOR LOGIN PURPOSES */
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
+
+    private ProgressDialog progressDialog;
+    ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+
+
         Log.d(TAG, "onCreate: Started.");
 
         sectionStatePagerAdapter = new SectionStatePagerAdapter(getSupportFragmentManager());
@@ -34,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         setUpViewPager(viewPager);
     }
 
-    private void setUpViewPager(ViewPager viewPager){
+    private void setUpViewPager(ViewPager viewPager) {
         SectionStatePagerAdapter adapter = new SectionStatePagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new Login(), "Login");
         adapter.addFragment(new Menu(), "Menu");
@@ -45,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
     }
 
-    public void setViewPager(int fragmentNumber){
+    public void setViewPager(int fragmentNumber) {
         viewPager.setCurrentItem(fragmentNumber);
     }
 
@@ -53,5 +99,88 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         viewPager.setCurrentItem(1);
     }
+
+
+    private void userLogin() {
+
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum lenght of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+
+                if(task.isSuccessful()) {
+                    finish();
+                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(this, ProfileActivity.class));
+
+        }
+    }
+
+    // REMEMBER LOGIN CLASS IS RESPONSBLE FOR HANDLING SIGNUP
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textViewSignin:
+            finish();
+            startActivity(new Intent(this, Login.class));
+            break;
+
+            case R.id.buttonLogin:
+                userLogin();
+                break;
+
+        }
+    }
+
+
 }
-//
---
+++ app/src/main/java/oakberg/dk/mytemplate/fragment/ProfileActivity.java
@@ -0,0 +1,184 @@
+package oakberg.dk.mytemplate.fragment;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+import oakberg.dk.mytemplate.R;
+import oakberg.dk.mytemplate.activities.MainActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private static final int CHOOSE_IMAGE = 101;
+
+    TextView textView;
+    ImageView imageView;
+    EditText editText;
+
+    Uri uriProfileImage;
+    ProgressBar progressBar;
+
+    String profileImageUrl;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        editText = (EditText) findViewById(R.id.editTextDisplayName);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        textView = (TextView) findViewById(R.id.textViewVerified);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showImageChooser();
+            }
+        });
+
+        loadUserInformation();
+
+        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                saveUserInformation;
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+
+    }
+
+    private void loadUserInformation() {
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user != null) {
+            if (user.getPhotoUrl() != null) {
+                Glide.with(this)
+                        .load(user.getPhotoUrl().toString())
+                        .into(imageView);
+            }
+
+            if (user.getDisplayName() != null) {
+                editText.setText(user.getDisplayName());
+            }
+
+            if (user.isEmailVerified()) {
+                textView.setText("Email verified");
+
+            } else {
+
+                textView.setText("Email not verified (click to verify)");
+                textView.setOnClickListener(new View.OnClickListener() {
+
+                    public void onClick(View view) {
+                        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(ProfileActivity.this, "Verification Email Sent", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+                    }
+
+                });
+
+            }
+        }
+    }
+
+    private void saveUserInformation() {
+
+        String displayName = editText.getText().toString();
+
+        if (displayName.isEmpty()) {
+            editText.setError("Name required");
+            editText.requestFocus();
+            return;
+        }
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        // If the user and profile cant be found, he will be prompted to set photo and displayname
+
+        if (user != null && profileImageUrl != null) {
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(displayName)
+                    .setPhotoUri(Uri.parse(profileImageUrl))
+                    .build();
+
+            // Updates the profile and tells the user accordingly
+
+            user.updateProfile(profile)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ProfileActivity.this, "Profile Updated", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
+
+    }
+
+    private void showImageChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Profile Image"), CHOOSE_IMAGE);
+    }
+
+}
+
+
+
+
+
+
+
+
