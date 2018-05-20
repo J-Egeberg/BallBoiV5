e377eb97fdfbcc65731baef1a25aca116c89dee5
cymback <caspergrozz@gmail.com>
2018-05-20 14:05:58 +0200

    Added Allrecipes, recipes listview etc

--- app/src/main/java/oakberg/dk/mytemplate/fragment/AllRecipes.java
+++ app/src/main/java/oakberg/dk/mytemplate/fragment/AllRecipes.java
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import oakberg.dk.mytemplate.R;
+import oakberg.dk.mytemplate.entity.Recipe;
+import oakberg.dk.mytemplate.entity.RecipeAdapter;
+
 
 /**
  * Created by Oakberg on 06/04/2018.
@@ -15,10 +22,21 @@ import oakberg.dk.mytemplate.R;
 
 public class AllRecipes extends Fragment {
 
+    private ListView mListView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.allrecipes_fragment_layout, container, false);
+
+        mListView = (ListView) view.findViewById(R.id.recipe_list_view);
+
+        // Loads list of objects from the jSON file in asset folder
+
+        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", getActivity());
+
+        RecipeAdapter adapter = new RecipeAdapter(getActivity(), recipeList);
+        mListView.setAdapter(adapter);
 
 
         return view;
